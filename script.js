eb6bcdd0c4c52662090dbc0c280f9a42fbbd0bec
hurihidayat12 <hurihidayat2@gmail.com>
2022-01-10 15:45:53 +0700

    finish minggu ke-2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//take data from Id
 let blogs = [];
 
 function addBlog(event){
@@ -64,7 +65,7 @@ function renderBlog() {
               ${blogs[i].title}</a>
           </h1>
           <div class="detail-blog-content">
-              ${blogs[i].postAt} | ${blogs[i].author}
+              ${getfullTime(blogs[i].postAt)} | ${blogs[i].author}
           </div>
             <p>
               ${blogs[i].content}
@@ -108,26 +109,31 @@ function getfullTime(time) {
   let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`
 
   console.log(fullTime)
-  return getfullTime;
+  return fullTime;
 }
 
-function getDistanceTime(time) {
+function getDistanceTime(huri) {
 
-  let timePost = time;
+  let timePost = huri;
   let timeNow = new Date();
+
+
   
   let distance = timeNow - timePost;
 
+  //variable penampung waktu
   let miliSecond =  1000; // = 1 detik
   let secondinHours = 3600; //1jam = 3600 mili second
   let hoursinDay = 23; //23 jam dalam 1 hari
   let minutes = 60;
   let seconds = 60;
 
-  let distanceDay = Math.floor(distance / (miliSecond * secondinHours * hoursinDay)); //untuk mendapakatkan hari
-  let distanceHours  = Math.floor(distance / (miliSecond * seconds  * minutes )); //untuk mendapatkan jam
-  let distanceMinutes = Math.floor(distance / (miliSecond * seconds))
-  let distanceSecond = Math.floor(distance / miliSecond)
+
+//untuk mendapakatkan hari, jam, menit dan detik
+  let distanceDay = Math.floor(distance / (miliSecond * secondinHours * hoursinDay)); 
+  let distanceHours  = Math.floor(distance / (miliSecond * seconds  * minutes )); 
+  let distanceMinutes = Math.floor(distance / (miliSecond * seconds));
+  let distanceSecond = Math.floor(distance / miliSecond);
   
   //distanceDay = Math.floor(distanceDay);
   //console.log(distanceDay+' day ago');
@@ -148,10 +154,88 @@ function getDistanceTime(time) {
 
 setInterval(() => {
   renderBlog();
- },5000);
+ },3600);
+
+//finish blog sourcode
 
 
 
+//SourCode Contact
 
+ function submitData() {
+  let name = document.getElementById("input-username").value;
+  let email = document.getElementById("input-gmail").value;
+  let phone = document.getElementById("input-phone").value;
+  let fullDev = document.getElementById("Full").checked;
+  let frontDev = document.getElementById("front").checked
+  let backDev = document.getElementById("back").checked;
+  let subject = document.getElementById("input-subject").value;
+  let text = document.getElementById("input-text").value;
 
+  if (fullDev){
+      fullDev = (document.getElementById("Full").value);
+  } 
+  else {
+      fullDev = ""
+  }
+
+  if (frontDev){
+      frontDev = (document.getElementById("front").value);
+  }
+  else {
+      frontDev = ""
+  }
+
+  if (backDev){
+      backDev = (document.getElementById("back").value)
+  }
+  else {
+      backDev = ""
+  }
+
+  console.info(name);
+  console.info(email);
+  console.info(phone);
+  console.info(fullDev);
+  console.info(frontDev);
+  console.info(backDev);
+  console.info(subject);
+  console.info(text);
+
+//form condition
+  if (name == ""){
+      return alert ("Plese Enter Your Name in the Input Form !!");
+  }
+  else if (email == ""){
+      return alert("the email section Must be Filled in!!");
+  }
+  else if (phone == ""){
+      return alert("Please Input Your Number Phone !!");
+  }
+  else if (subject == ""){
+      return alert("Please Select Subject !!");
+  }
+  else if(text == ""){
+      return alert("I am Sory, Please Input Your Message Before Send !!");
+  }
+
+  let emailSend = 'loper@gmail.com'
+  let a = document.createElement('a');
+
+  a.href= `mailto: ${emailSend}?subject=${subject}&body=Assalamualaikum Hallo ${name} 
+  nomor telephone  : ${phone} send task project ke ${email} tugas nya ${text} khusus 
+  bagian ${fullDev} ${backDev} ${frontDev}`
+  a.click();
+
+  let grup = {
+      name: name, 
+      email: email,
+      telphone: phone,
+      subject: subject,
+      text: text,
+      options: frontDev, backDev, fullDev,
+  }
+  console.log(grup);
+
+}
 
